07b7de4cfd50fd1d1be43889a870a434d69d9b94
BuddhimaDilini <buddhimadilini0819@gmail.com>
2024-12-25 14:45:55 +0530

    SID AJAX

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -21,6 +21,34 @@ $(document).ready(function () {
 
     if (searchOption == "id") {
       // ajax call to get student by id
+      $.ajax({
+        url: "http://localhost:3000/students/searchSID/" + searchText,
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data, textStatus, jQXhr) {
+          let student = data[0];
+          let studentCard = `
+              <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h3 class="card-title">${student.firstName} ${student.lastName}</h3>
+                  <p class="card-text">
+                    <strong>ID:</strong> ${student.sid}<br>
+                    <strong>Email:</strong> ${student.email}<br>
+                    <strong>City:</strong> ${student.nearCity}<br>
+                    <strong>Course:</strong> ${student.course}<br>
+                    <strong>Guardian:</strong> ${student.guardian}<br>
+                    <strong>Subjects:</strong> ${student.subjects}<br>
+                  </p>
+                </div>
+              </div>
+            `;
+          $(".student-details").html(studentCard);
+        },
+        error: function (jQXhr, textStatus, errorThrown) {
+          alert("Student not found");
+        },
+      });
 
     } else if (searchOption == "firstName") {
       // ajax call to get student by first name
