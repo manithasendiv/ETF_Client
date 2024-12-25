49c69f4a73f1dc57fb14818a76ebdd7c5e833ad1
BuddhimaDilini <buddhimadilini0819@gmail.com>
2024-12-25 15:23:09 +0530

    Email AJAX

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,8 +16,10 @@ $(document).ready(function () {
 
   // Home page - search student JS
   $("#searchButton").click(function () {
+    event.preventDefault();
     let searchOption = document.getElementById("searchBySelect").value;
     let searchText = document.getElementById("searchInput").value;
+    console.log(searchOption,searchText);
 
     if (searchOption == "id") {
       // ajax call to get student by id
@@ -58,6 +60,34 @@ $(document).ready(function () {
 
     } else if (searchOption == "email") {
       // ajax call to get student by email
+      $.ajax({
+        url: "http://localhost:3000/students/searchEmail/" + searchText,
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data, textStatus, jQXhr) {
+          let student = data[0];
+          let studentCard = `
+              <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h3 class="card-title">${student.firstName} ${student.lastName}</h3>
+                  <p class="card-text">
+                    <strong>ID:</strong> ${student.sid}<br>
+                    <strong>Email:</strong> ${student.email}<br>
+                    <strong>City:</strong> ${student.nearCity}<br>
+                    <strong>Course:</strong> ${student.course}<br>
+                    <strong>Guardian:</strong> ${student.guardian}<br>
+                    <strong>Subjects:</strong> ${student.subjects}<br>
+                  </p>
+                </div>
+              </div>
+            `;
+          $(".student-details").html(studentCard);
+        },
+        error: function (jQXhr, textStatus, errorThrown) {
+          alert("Student not found");
+        },
+      });
 
     } else if (searchOption == "city") {
       // ajax call to get student by city
